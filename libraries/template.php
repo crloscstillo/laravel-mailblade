66234daea17356e1848284125f241c3104cc4c5f
Carlos Castillo <carlos@koalabs.co>
2013-03-29 15:58:19 -0500

    Added __construct method, and params and set_template methods

--- libraries/template.php
+++ libraries/template.php
@@ -38,9 +38,15 @@ class Template {
    * @param   array   $params
    * @return  void
    */
-  public function __construct($name=null, array $params=null)
+  public function __construct($name, array $params = NULL)
   {
-    
+    // Set name
+    $this->name = $name;
+    // Set parameters
+    if ($params) $this->params = $params;
+    // Get contents of template
+    $this->set_template('html');
+    $this->set_template('txt');
   }
 
   /**
@@ -50,9 +56,53 @@ class Template {
    * @param  array  $params
    * @return Template
    */
-  public static function make($name=null, array $params=null)
+  public static function make($name, array $params = NULL)
   {
     return new static($name, $params);
+  }
+
+  #             ~ ---------- ~              #
+  
+  /**
+   * Set ALL parameters for the email template
+   * 
+   * @param  array  $params
+   * @return Template
+   */
+  public function parameters(array $params)
+  {
+    $this->params = $params;
+    return $this;
+  }
+
+  /**
+   * Set a parameter for the template.
+   * 
+   * @param  string $param
+   * @param  string $value
+   * @return Template
+     */
+  public function with($param, $value)
+  {
+    $this->params[$param] = $value;
+    return $this;
+  }
+
+  #             ~ ---------- ~              #
+  
+  /**
+   * Set appropriate email template content
+   * 
+   * @param   string    $type
+   * @return  Template
+   */
+  private function set_template($type)
+  {
+    $dir  = 'templates';
+    $lang = Config::get('application.language');
+    $file = Bundle::path('mailblade').$dir.DS.$lang.DS.$this->name.'.'.$type;
+    $this->$type = file_get_contents($file);
+    return $this;
   }
 
 }
