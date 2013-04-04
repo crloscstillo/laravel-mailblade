0bb66d25ee7554fe431e390014d06779bf94b4a4
Carlos Castillo <carlos@koalabs.co>
2013-04-03 23:06:34 -0500

    Added the SMTP class

---
+++ config/smtp-sample.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Contacto >> SMTP configuration
+|--------------------------------------------------------------------------
+*/
+
+return array(
+
+  // Debug mode will echo connection status alerts to
+  // the screen throughout the email sending process.
+  // Very helpful when testing your credentials.
+  
+  'debug_mode' => false,
+  
+  // Define the different connections that can be used.
+  // You can set which connection to use when you create
+  // the SMTP object: ``$mail = new SMTP('my_connection')``.
+
+  'default' => 'primary',
+
+  'connections' => array(
+    'primary' => array(
+      'host'   => 'smtp.gmail.com',
+      'port'   => '465',
+      'secure' => 'ssl', // null, 'ssl', or 'tls'
+      'auth'   => true, // true if authorization required
+      'user'   => '',
+      'pass'   => '',
+    ),
+  ),
+
+);
