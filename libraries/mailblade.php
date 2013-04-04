319237b8ea99ee5ce27cf5f8dc867a94381ef2d6
Carlos Castillo <carlos@koalabs.co>
2013-04-04 12:08:07 -0500

    Construct class

--- libraries/mailblade.php
+++ libraries/mailblade.php
@@ -1,4 +1,4 @@
-<?php
+<?php use Laravel\FilesystemIterator as fIterator; use Laravel\Closure as Closure;
 
 /**
  * Mailblade class
@@ -13,6 +13,60 @@
  */
 
 class Mailblade {
-  
+
+  /**
+   * The same compiler functions used by Blade.
+   *
+   * @var array
+   */
+  protected static $compilers = array(
+    'extensions',
+    'layouts',
+    'comments',
+    'echos',
+    'forelse',
+    'empty',
+    'endforelse',
+    'structure_openings',
+    'structure_closings',
+    'else',
+    'unless',
+    'endunless',
+    'includes',
+    'render_each',
+    'render',
+    'yields',
+    'yield_sections',
+    'section_start',
+    'section_end',
+  );
+
+  #             ~ ---------- ~              #
+
+  /**
+   * Create a new Template instance
+   *
+   * @param   string  $name
+   * @param   array   $params
+   * @return  void
+   */
+  public function __construct($name, array $params = NULL)
+  {
+
+  }
+
+  /**
+   * Create a new Template instance
+   * 
+   * @param  string $name  
+   * @param  array  $params
+   * @return Template
+   */
+  public static function make($name, array $params = NULL)
+  {
+    return new static($name, $params);
+  }
+
+  #             ~ ---------- ~              #
 
 }
