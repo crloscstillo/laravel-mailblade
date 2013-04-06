ef03ffc76910eedf2286143a33d3f4ea177c428d
Carlos Castillo <carlos@koalabs.co>
2013-04-05 21:36:01 -0500

    Added options sample

---
+++ config/options-sample.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+
+  /*
+  |--------------------------------------------------------------------------
+  | Template directory
+  |--------------------------------------------------------------------------
+  |
+  | You can store your email messages in any folder you want.
+  | Leave blank if you want to use Mailblade's default location.
+  | Set a custom folder and we will use that instead.
+  |
+  | Example:
+  |   'template_dir' => path('app').'templates'.DS
+  |
+  | If you want to point to a folder named 'templates' inside the
+  | application directory.
+  |
+  */
+ 
+  'template_dir' => ''
+
+);
