adcaef1978eb9f39943ad64e6eaf5fcaf6eb9b5a
Carlos Castillo <carlos@koalabs.co>
2013-04-06 08:49:32 -0500

    Explained default way of handling templates

--- config/options-sample.php
+++ config/options-sample.php
@@ -7,14 +7,15 @@ return array(
   | Template directory
   |--------------------------------------------------------------------------
   |
-  | You can store your email messages in any folder you want.
+  | Mailblade loads your email templates from the 'mailblade/views' folder but
+  | you can store your email messages in any folder you want.
   | Leave blank if you want to use Mailblade's default location.
   | Set a custom folder and we will use that instead.
   |
   | Example:
-  |   'template_dir' => path('app').'templates'.DS
+  |   'template_dir' => path('app').'views'
   |
-  | If you want to point to a folder named 'templates' inside the
+  | If you want to point to the 'views' folder inside the
   | application directory.
   |
   */
